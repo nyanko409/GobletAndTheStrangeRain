212bc14a28efe7414fc37b5c913e8b9455e7b6a4
Ottey0304 <kadokiti58@gmail.com>
2020-03-12 16:53:17 +0900

    Testing gimmick creation

---
+++ Shizuku/Assets/TomokiTemp/Ground.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    GameObject ground;
+
+    // todo：強引な方法を使っているので修正
+    int groundColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundColor = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スペースキー押下で色を赤青交互に切り替える
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (groundColor == 0) {
+                groundColor = 1;
+                GetComponent<Renderer>().material.color = Color.blue;
+            }
+
+            if (groundColor == 1) {
+                groundColor = 0;
+                GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+}
