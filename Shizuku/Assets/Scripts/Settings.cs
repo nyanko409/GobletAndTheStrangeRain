0bc9ce1a882cbb0b82448a992e3859e149947498
ato <ishimoto_ato@me.com>
2020-03-25 17:53:47 +0900

    add settings as scriptable object

---
+++ Shizuku/Assets/Scripts/Settings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameSettings settings;
+
+
+    private void Awake()
+    {
+        if (!settings) return;
+
+        Application.targetFrameRate = settings.targetFPS;
+    }
+}
---
+++ Shizuku/Assets/Scripts/_ScriptableObjects/GameSettings.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObject/GameSettings", order = 1)]
+public class GameSettings : ScriptableObject
+{
+    public int targetFPS;
+}
