aac1fa2381168c563c2df07423672d5b0a304ba0
kobayashi <ayesha.xiv@gmail.com>
2020-03-26 18:05:05 +0900

    addSceneTrans

---
+++ Shizuku/Assets/KazukiFile/SceneTrans/SSTrans.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class SSTrans : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
