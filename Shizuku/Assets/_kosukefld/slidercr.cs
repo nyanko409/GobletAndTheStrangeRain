65df06c73375a10d8719c6506c207788d4d61218
Rovari <k.kousuke._x_b@icloud.com>
2020-04-17 04:07:16 +0900

    skirwakatta

---
+++ Shizuku/Assets/_kosukefld/slidercr.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class slidercr : MonoBehaviour
+{
+    Slider _slider;
+
+    void Start()
+    {
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    //value max
+    float max = 10;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            
+
+                max -= 1;
+            
+
+        }
+
+        if(Input.GetKey(KeyCode.T))
+        {
+            max = 10;
+        }
+
+        // value set
+        _slider.value = max;
+    }
+}
