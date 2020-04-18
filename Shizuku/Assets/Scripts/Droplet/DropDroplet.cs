e3292efac2519f4528474dae2e0fa1d8a3b80aa7
ato <ishimoto_ato@me.com>
2020-04-18 17:06:13 +0900

    basic water over goblet

--- Shizuku/Assets/Scripts/Droplet/DropDroplet.cs
+++ Shizuku/Assets/Scripts/Droplet/DropDroplet.cs
@@ -2,6 +2,8 @@
 
 public class DropDroplet : MonoBehaviour
 {
+    public GameObject water;
+
     RippleData? data;
     GameInput action;
 
@@ -11,6 +13,11 @@ public class DropDroplet : MonoBehaviour
         // init actions
         action = new GameInput();
         action.Player.DropDroplet.performed += context => Drop();
+    }
+
+    private void Start()
+    {
+        water.SetActive(false);
     }
 
     private void Drop()
@@ -25,6 +32,8 @@ public class DropDroplet : MonoBehaviour
                 {
                     receiver.ApplyEffect(hit.point, data.Value.color, data.Value.spreadSpeed);
                     data = null;
+
+                    water.SetActive(false);
                 }
             }
         }
@@ -32,14 +41,19 @@ public class DropDroplet : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        RippleEffectProvider prov;
-        other.gameObject.TryGetComponent(out prov);
-        if(prov)
+        RippleEffectProvider provider;
+        if (other.gameObject.TryGetComponent(out provider))
         {
+            // copy the ripple data to local variable
             RippleData d = new RippleData();
-            d.color = prov.RippleColor;
-            d.spreadSpeed = prov.RippleSpreadSpeed;
+            d.color = provider.RippleColor;
+            d.spreadSpeed = provider.RippleSpreadSpeed;
             data = d;
+
+            // change the water color
+            water.SetActive(true);
+            water.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", d.color);
+            water.GetComponent<MeshRenderer>().material.SetColor("_RippleColor", d.color);
         }
     }
 
