40739bfafeaa142dd469d86c6b2fd435e63dec78
ato <ishimoto_ato@me.com>
2020-04-24 15:04:37 +0900

    delete unnessecary raycast

--- Shizuku/Assets/Scripts/Camera/CameraFollow.cs
+++ Shizuku/Assets/Scripts/Camera/CameraFollow.cs
@@ -8,12 +8,6 @@ public class CameraFollow : MonoBehaviour
 
     private void LateUpdate()
     {
-        if(Physics.Raycast(transform.position, -transform.forward, out RaycastHit hit, 10))
-        {
-            transform.position = Vector3.Lerp(transform.position, target.position, smoothSpeed * Time.deltaTime);
-
-        }
-        else
-            transform.position = Vector3.Lerp(transform.position, target.position, smoothSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, target.position, smoothSpeed * Time.deltaTime);
     }
 }
