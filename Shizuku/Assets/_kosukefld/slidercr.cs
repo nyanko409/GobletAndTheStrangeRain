bd4b2f612a671f08d444c295f04177e343d37370
Rovari <k.kousuke._x_b@icloud.com>
2020-04-25 14:47:40 +0900

    sukirumazi

--- Shizuku/Assets/_kosukefld/slidercr.cs
+++ Shizuku/Assets/_kosukefld/slidercr.cs
@@ -1,26 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
-public class slidercr : MonoBehaviour
+public class Slidercr : MonoBehaviour
 {
-    
-    
+    //zenzai
+    public DropDroplet sabu;
 
+    //kako
+    Color cl;
+
+    //sub
+    Color cl2;
 
     public RawImage sliderImage;
 
     Slider _slider;
 
-    //color
-    GameObject fill;
+
+
+
+
+
+
+
 
     void Start()
     {
+
+
+
         _slider = GameObject.Find("Slider").GetComponent<Slider>();
 
-        //color get
-        fill = GameObject.Find("Fill");
+
 
         _slider.maxValue = 5;
 
@@ -33,41 +45,37 @@ public class slidercr : MonoBehaviour
     //0=start 1=in 2=out
     int colorset = 0;
 
-    //1=red 2=blue
-    int colorCode = 1;
 
-    //red
-    byte CL_R = 0;
+    float clcount = 0;
 
-    //green
-    byte CL_G = 0;
-
-    //blue
-    byte CL_B = 0;
 
     void Update()
     {
 
-        //water in
-        if (Input.GetKeyDown(KeyCode.R))
+
+        if (!sabu.HasWater())
         {
+
             colorset = 1;
         }
-
-        //water out
-        if (Input.GetKeyDown(KeyCode.T))
+        else
         {
+
+            cl = sabu.GetColor();
             colorset = 2;
         }
 
+
         //watercontrol
-            switch (colorset)
+        switch (colorset)
         {
             case 1:
+
                 if (max > _slider.minValue)
                 {
 
                     max -= 0.025f;
+
                 }
                 break;
 
@@ -77,79 +85,45 @@ public class slidercr : MonoBehaviour
 
                     max += 0.025f;
                 }
+
                 break;
 
             default:
                 break;
         }
 
-        sliderImage.color = new Color32(CL_R, CL_G, CL_B, 255);
 
-        //colorcontrol
+        sliderImage.color = cl;
+
+
+
+        //if (clcount < 1)
+        //{
+
+        //    clcount += 0.1f;
+
+        //}
+
+        //sliderImage.color = Color.Lerp(cl, sabu.GetColor(), clcount);
+
+
+
+
+
         if (Input.GetKeyDown(KeyCode.Y))
         {
-            if (colorCode == 1)
-            {
-                colorCode = 2;
-            } else if (colorCode == 2)
-            {
-                colorCode = 1;
-            }
+            Debug.Log(cl);
         }
-        switch(colorCode)
+        if (Input.GetKeyDown(KeyCode.U))
         {
-            case 1://red
-                if(CL_R<255)
-                {
-                    CL_R += 1;
-                }
-                if(CL_G>0)
-                {
-
-                    CL_G -= 1;
-                }
-                if (CL_B > 0)
-                {
-
-                    CL_B -= 1;
-                }
-
-                break;
-            case 2://blue
-                if (CL_R > 0)
-                {
-
-                    CL_R -= 1;
-                }
-
-                if (CL_G < 255)
-                {
-                    CL_G += 1;
-                }
-                if (CL_B < 255)
-                {
-                    CL_B += 1;
-                }
-                break;
-            default:
-                break;
+            Debug.Log(sabu.GetColor());
         }
-        //sliderImage.color = new Color32(CL_R,CL_G,CL_B, 255);
-        //if(Input.GetKeyDown(KeyCode.Y))
-        //{
-        //    if (nextcolorCode == 1)
-        //    {
-        //        sliderImage.color = new Color32(255, 0, 0, 255);
 
-        //        nextcolorCode = 2;
 
-        //    }else if(nextcolorCode==2)
-        //    {
-        //        sliderImage.color = new Color32(0, 255, 255, 255);
 
-        //        nextcolorCode = 1;
-        //    }
-        //}
+
+
+
 
         // value set
         _slider.value = max;
