1ca53b8291c75cdc2e7826b95d69a0c1e8c40bf4
ato <ishimoto_ato@me.com>
2020-04-26 12:51:45 +0900

    fix duplication of loading screen by mashing the button

--- Shizuku/Assets/Scripts/UI and HUD/SceneLoader.cs
+++ Shizuku/Assets/Scripts/UI and HUD/SceneLoader.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public abstract class SceneLoader : MonoBehaviour
 {
+    private static bool isLoading = false;
+
+
     public static IEnumerator LoadSceneAsync(string sceneName, string prefabPath, float simulatedTime = -1)
     {
-        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
-        Instantiate(Resources.Load(prefabPath));
+        // prevent loading the scene multiple times by mashing the button
+        if (!isLoading)
+        {
+            isLoading = true;
 
-        if (simulatedTime >= 0)
-        {
-            op.allowSceneActivation = false;
-            yield return new WaitForSeconds(simulatedTime);
-            op.allowSceneActivation = true;
-        }
-        else
-        {
-            while (!op.isDone)
-                yield return null;
+            // load the scene in the background
+            AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+            Instantiate(Resources.Load(prefabPath));
+
+            // switch scene if the scene is finished loading
+            if (simulatedTime >= 0)
+            {
+                op.allowSceneActivation = false;
+                yield return new WaitForSeconds(simulatedTime);
+                op.allowSceneActivation = true;
+            }
+            else
+            {
+                while (!op.isDone)
+                    yield return null;
+            }
+
+            isLoading = false;
         }
     }
 }
