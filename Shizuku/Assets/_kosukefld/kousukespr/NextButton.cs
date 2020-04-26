f90fb6e275717ac9d7ff5427326898a1cc061fbc
Rovari <k.kousuke._x_b@icloud.com>
2020-04-27 08:23:12 +0900

    Titleimage

---
+++ Shizuku/Assets/_kosukefld/kousukespr/NextButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextButton : MonoBehaviour
+{
+    public TitleUI check;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+       if( Input.GetKeyDown(KeyCode.JoystickButton0) == true && check.BuuttonCK()==true)
+        {
+            StartCoroutine(SceneLoader.LoadSceneAsync("StageSelect", "Prefabs/UI and HUD/Loading Canvas", 3));
+            Debug.Log("osita A");
+        }
+    }
+}
