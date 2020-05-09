238c7125f7d2a192858e331b62509cb06884eb65
Rovari <k.kousuke._x_b@icloud.com>
2020-05-09 18:27:14 +0900

    New Image

---
+++ Shizuku/Assets/_kosukefld/XBu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XBu : MonoBehaviour
+{
+   public PlayerController data;
+   public Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.color = new Color32(255, 255, 255, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!data.IsInDragRange())
+        {
+            image.color = new Color32(255, 255, 255, 0);
+        }
+        else
+        {
+            image.color = new Color32(255, 255, 255, 255);
+        }
+        Debug.Log(data.IsInDragRange());
+        
+    }
+}
