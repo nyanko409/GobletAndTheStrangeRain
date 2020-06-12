19f55c686dbd5d2d0127463f00bdc04989a90231
ato <ishimoto_ato@me.com>
2020-06-12 13:51:31 +0900

    new game and continue now working

---
+++ Shizuku/Assets/Scripts/Other/GameData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public Stage[] stageData;
+
+    public void ResetGameData()
+    {
+        for(int i = 1; i < stageData.Length; ++i)
+        {
+            stageData[i].isLocked = true;
+        }
+    }
+}
