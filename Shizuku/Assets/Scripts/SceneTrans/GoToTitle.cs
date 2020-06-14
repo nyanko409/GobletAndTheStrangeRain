e580701ae1ccd02be41cf3083174350207e0fe75
kobayashi <ayesha.xiv@gmail.com>
2020-06-14 21:14:03 +0900

    clear kara  title

--- Shizuku/Assets/Scripts/SceneTrans/GoToTitle.cs
+++ Shizuku/Assets/Scripts/SceneTrans/GoToTitle.cs
@@ -3,21 +3,24 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
+
 public class GoToTitle : MonoBehaviour
 {
-
     void Update()
-    {       
-        if (Input.GetKeyDown(KeyCode.Space))
+    {
+
+
+        if (Input.GetKeyDown("joystick button 0"))
         {
             FadeManager.FadeOut("Title");
         }
     }
 
-    // Start is called before the first frame update
+        // Start is called before the first frame update
     void Start()
     {
         FadeManager.FadeIn();
     }
+
 }
 
