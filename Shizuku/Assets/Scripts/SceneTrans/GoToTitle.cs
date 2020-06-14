246320baddc3a0f6f5ca477869f568fe45b5f767
kobayashi <ayesha.xiv@gmail.com>
2020-06-14 20:52:57 +0900

    add gameclear

---
+++ Shizuku/Assets/Scripts/SceneTrans/GoToTitle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GoToTitle : MonoBehaviour
+{
+
+    void Update()
+    {       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FadeManager.FadeOut("Title");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeManager.FadeIn();
+    }
+}
+
